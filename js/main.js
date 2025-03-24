620f454aeffdf0aa9cc8eafe19b2337a33e3ce1f
takeru-k <kam41893@gmail.com>
2025-03-24 09:36:39 +0900

    全体修正

--- js/main.js
+++ js/main.js
@@ -6,18 +6,39 @@ $(function () {
 });
 
 // toggle
-
+$(function(){
 	$('.c-toggle__header').click(function(){
 		$(this).toggleClass('selected');
 		$(this).next().slideToggle();
 	});
+});
 
-
-
-    let tabs = $(".c-tab__item");
+   let tabs = $(".c-tab__item");
     $(".c-tab__item").on("click", function() {
         $(".active").removeClass("active");
         $(this).addClass("active");
         const index = tabs.index(this);
         $(".content").removeClass("show").eq(index).addClass("show");
     });
+
+$(function(){
+  $('.p-top-service__slider').slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 6000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 4, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1.5, // 画面幅750px以下でスライド3枚表示
+        }
+      }
+    ]
+  });
+});
